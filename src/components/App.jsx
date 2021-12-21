61324fce6457ed1c355890acd23a1c324a117eb9
milesmaroney <miles@milesmaroney.com>
2021-12-21 15:25:12 -0800

    Initial rating branch commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RatingsAndReviews from './RatingsReviews/RatingsAndReviews'
 
 export default function App(props) {
   return (
@@ -11,6 +12,7 @@ export default function App(props) {
         <button type="button" className="btn btn-primary">
           This is a bootstrap button
         </button>
+        <RatingsAndReviews />
       </>
   )
 }
