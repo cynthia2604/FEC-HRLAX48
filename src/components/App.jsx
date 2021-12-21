e8ad73195611af5efb5543e846c532bc145ad8df
milesmaroney <miles@milesmaroney.com>
2021-12-21 15:22:57 -0800

    Initial Skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App(props) {
+  return (
+      <>
+        <h1>
+          Hello!
+        </h1>
+        <button type="button" className="btn btn-primary">
+          This is a bootstrap button
+        </button>
+      </>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  }
+};
+
+module.exports = config;
