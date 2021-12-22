56eba828f7023e47c1612f8e0c299c54e0c94043
emilyhu08 <emilyhu08@gmail.com>
2021-12-22 14:35:10 -0800

    add id in review and scroll to element to review session

--- src/components/Overview/ProductInfo.jsx
+++ src/components/Overview/ProductInfo.jsx
@@ -5,10 +5,20 @@ import AddToBag from "./AddToBag";
 import utils from "../utils.js";
 
 export default function ProductInfo({ productDetail, productStyles, rating }) {
+  const handleScroll = () => {
+    const reviewElement = document.getElementById("review");
+    reviewElement.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div>
-      <span>{utils.starRating(rating)}</span>
-      <span>Read All Reviews</span>
+      <div className="d-inline-flex">
+        <span>{utils.starRating(rating)}</span>
+        <span>
+          <u onClick={handleScroll}>Read All Reviews</u>
+        </span>
+      </div>
+
       <p className="pd__category">{productDetail.category}</p>
       <h2 className="pd__name">{productDetail.name}</h2>
       <p className="pd__price">{"$" + productDetail.default_price}</p>
--- src/components/RatingsReviews/index.jsx
+++ src/components/RatingsReviews/index.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
-import axios from 'axios'
-import Options from '../../config'
-import StarColumn from './StarColumn'
-import ReviewColumn from './ReviewColumn'
-import utils from '../utils'
+import React from "react";
+import axios from "axios";
+import Options from "../../config";
+import StarColumn from "./StarColumn";
+import ReviewColumn from "./ReviewColumn";
+import utils from "../utils";
 
 export default function RatingsAndReviews(props) {
-
-  const [productInfo, setProductInfo] = React.useState()
-  const [percentage, setPercentage] = React.useState(100)
+  const [productInfo, setProductInfo] = React.useState();
+  const [percentage, setPercentage] = React.useState(100);
 
   React.useEffect(() => {
-    axios.get(`${Options.URL}/reviews/?product_id=${props.selected.id}&count=10`, {
-      headers: {
-        Authorization: Options.TOKEN
-      }
-    })
-      .then(res => setProductInfo(res.data))
-  }, [props.selected])
-
+    axios
+      .get(`${Options.URL}/reviews/?product_id=${props.selected.id}&count=10`, {
+        headers: {
+          Authorization: Options.TOKEN,
+        },
+      })
+      .then((res) => setProductInfo(res.data));
+  }, [props.selected]);
 
   return (
     <>
-      <div className='sectionTitle'>
-        {`RATINGS & REVIEWS`}
-      </div>
-      {productInfo &&
-      <div className='reviews'>
-        <div className='reviews-left'>
-          <StarColumn rating={props.rating} productInfo={productInfo} starRating={utils.starRating}/>
+      <div className="sectionTitle">{`RATINGS & REVIEWS`}</div>
+      {productInfo && (
+        <div id="review" className="reviews">
+          <div className="reviews-left">
+            <StarColumn
+              rating={props.rating}
+              productInfo={productInfo}
+              starRating={utils.starRating}
+            />
+          </div>
+          <div className="reviews-right">
+            <ReviewColumn
+              rating={props.rating}
+              productInfo={productInfo}
+              starRating={utils.starRating}
+            />
+          </div>
         </div>
-        <div className='reviews-right'>
-          <ReviewColumn rating={props.rating} productInfo={productInfo} starRating={utils.starRating}/>
-        </div>
-      </div>
-      }
+      )}
     </>
-  )
+  );
 }
