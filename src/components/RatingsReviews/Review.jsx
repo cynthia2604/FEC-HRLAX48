39f42da840133b42647ae274f6aff5a76d3d816c
milesmaroney <miles@milesmaroney.com>
2021-12-22 10:35:58 -0800

    Some rating updates

--- src/components/RatingsReviews/Review.jsx
+++ src/components/RatingsReviews/Review.jsx
@@ -4,9 +4,18 @@ import moment from "moment"
 export default function Review(props) {
   console.log(props.review)
 
-  const check = <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" className="bi bi-check2" viewBox="0 0 16 16">
-  <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-</svg>
+  function recommended() {
+    if (props.review.recommend) {
+      return (
+        <div className="reviewRecommended">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" className="bi bi-check2" viewBox="0 0 16 16">
+            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+          </svg>
+          I recommend this product <button type="button" className="btn btn-light">Text For Button</button>
+        </div>
+      )
+    }
+  }
 
   return (
     <div className='review'>
@@ -24,9 +33,7 @@ export default function Review(props) {
       <div className="reviewBody">
         {props.review.body}
       </div>
-      <div className="reviewRecommended">
-        {props.review.recommend && check} {props.review.recommend ? `I recommend this product` : ''}
-      </div>
+        {recommended()}
       <div className='reviewDivider'></div>
     </div>
   )
