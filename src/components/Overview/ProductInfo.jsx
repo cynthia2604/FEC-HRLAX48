435c6a1b2f0b4e168a595985dee82043dd7e6919
milesmaroney <miles@milesmaroney.com>
2021-12-27 17:37:21 -0800

    Revert "hide star also when no review, add space and share link to share buttons"
    This reverts commit cd5977c924d68b001fc44bab31e12b4ab6261535.

--- src/components/Overview/ProductInfo.jsx
+++ src/components/Overview/ProductInfo.jsx
@@ -21,21 +21,18 @@ export default function ProductInfo({
 
   return (
     <div>
-      {reviews.results.length ? (
-        <div className="pd__box">
-          <div className="pd__normal mb-3">{utils.starRating(rating)}</div>
+      <div className="pd__box">
+        <div className="pd__normal mb-3">{utils.starRating(rating)}</div>
+        {reviews.results.length ? (
           <div className="pd__normal mb-3">
             <u onClick={handleScroll}>
               Read All {reviews.results.length} Reviews
             </u>
           </div>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
 
-      <p className="mb-4">
-        Category:
-        {" " + productDetail.category}
-      </p>
+      <p className="mb-4">{"Category: " + productDetail.category}</p>
       <h2 className="mb-2">{productDetail.name}</h2>
       <h3 className="mb-4">{"$" + productDetail.default_price}</h3>
       <StyleSelector
