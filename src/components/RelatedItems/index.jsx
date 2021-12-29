bb2dd1db15773a88182afded06dc47aa1b7840c3
Ernest Zhang <ernestzhang97@gmail.com>
2021-12-29 00:14:35 -0800

    tentative work on carousel effect for list behavior

--- src/components/RelatedItems/index.jsx
+++ src/components/RelatedItems/index.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import OutfitList from './OutfitList';
 import Compare from './Compare'
 import Options from '../../config.js';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 export default function RelatedItems(props) {
   const [relatedItems, setRelatedItems] = React.useState([]);
@@ -24,32 +26,36 @@ export default function RelatedItems(props) {
   return (
     <div className="related-products">
       <div className="sectionTitle">RELATED ITEMS</div>
-        <div className="related-product-list">
-          <RelatedList
-            selected={props.selected}
-            related ={relatedItems}
-            setRenderTable={setRenderTable}
-            renderTable={renderTable}
-            setSelectRelated={setSelectRelated}
-          />
-         {renderTable &&
-          <Compare
-            related={relatedItems}
-            renderTable={renderTable}
-            selected={props.selected}
-            selectRelated={selectRelated}
-          />
-         }
-        </div>
+        <Carousel >
+          <div className="related-product-list">
+            <RelatedList
+              selected={props.selected}
+              related ={relatedItems}
+              setRenderTable={setRenderTable}
+              renderTable={renderTable}
+              setSelectRelated={setSelectRelated}
+            />
+          {renderTable &&
+            <Compare
+              related={relatedItems}
+              renderTable={renderTable}
+              selected={props.selected}
+              selectRelated={selectRelated}
+            />
+          }
+          </div>
+        </Carousel>
       <div className="sectionTitle">YOUR OUTFITS</div>
-        <div className="user-created-outfit">
-          <OutfitList
-            currentView={props.selected}
-            saved={props.saved}
-            setSaved={props.setSaved}
-            outfits={props.outfits}
-          />
-        </div>
+        <Carousel>
+          <div className="user-created-outfit">
+            <OutfitList
+              currentView={props.selected}
+              saved={props.saved}
+              setSaved={props.setSaved}
+              outfits={props.outfits}
+            />
+          </div>
+        </Carousel>
     </div>
   )
 }
