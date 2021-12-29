f653126509c571ec4b1d28e163e8cc2cf7c9df72
emilyhu08 <emilyhu08@gmail.com>
2021-12-29 11:02:14 -0800

    move selectedStyle state to detail

--- src/components/Overview/index.jsx
+++ src/components/Overview/index.jsx
@@ -10,18 +10,15 @@ import OverviewInfo from "./OverviewInfo";
 import "../../styles.css";
 import { v4 as uuidv4 } from "uuid";
 
-export default function Overview({ selected, rating, productInfo }) {
+export default function Overview({
+  selected,
+  rating,
+  productInfo,
+  selectedStyle,
+  setSelectedStyle,
+}) {
   const [productDetail, setProductDetail] = React.useState({});
   const [productStyles, setProductStyles] = React.useState({});
-  const [selectedStyle, setSelectedStyle] = React.useState({
-    color: "",
-    skus: null,
-    photos: null,
-    thumbnails: null,
-    thumbnail: null,
-    originalPrice: null,
-    salePrice: null,
-  });
   const [isExpand, setIsExpand] = React.useState(false);
 
   React.useEffect(() => {
