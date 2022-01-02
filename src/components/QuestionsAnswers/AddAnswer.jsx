e9738ab75dafe97f3082fd2c993bca654e0a2cdf
milesmaroney <miles@milesmaroney.com>
2022-01-02 12:42:34 -0800

    Refactoring to using toasts instead of window alerts WIP

--- src/components/QuestionsAnswers/AddAnswer.jsx
+++ src/components/QuestionsAnswers/AddAnswer.jsx
@@ -1,142 +1,253 @@
-import React from "react";
-import axios from "axios";
-import Options from "../../config";
+import React from 'react';
+import axios from 'axios';
+import Options from '../../config';
+import {
+  Alert,
+  CircularProgress,
+  LinearProgress,
+  Snackbar,
+} from '@mui/material';
+import $ from 'jquery';
+import utils from '../utils';
+import { Box } from '@mui/system';
 
 export default function AddAnswer(props) {
-
-  const [question, setQuestion] = React.useState(props.question)
-  const [answer, setAnswer] = React.useState('')
-  const [nickname, setNickname] = React.useState('')
-  const [email, setEmail] = React.useState('')
-  const [photos, setPhotos] = React.useState([])
-  const [photosToUpload, setPhotosToUpload] = React.useState([])
+  const [question, setQuestion] = React.useState(props.question);
+  const [answer, setAnswer] = React.useState('');
+  const [nickname, setNickname] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [photos, setPhotos] = React.useState([]);
+  const [photosToUpload, setPhotosToUpload] = React.useState([]);
+  const [showError, setShowError] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('');
 
   function handleSubmit() {
-    let error = ['Errors:']
-    if (!nickname) {
-      error.push('Nickname Missing')
+    let error = [];
+    if (!answer) {
+      error.push('Answer Required');
     }
-    if (!validateEmail(email)) {
-      error.push('Invalid Email Format')
+    if (!nickname) {
+      error.push('Nickname Required');
+    }
+    if (!utils.validateEmail(email)) {
+      error.push('Invalid Email Format');
     }
     if (photos.length !== photosToUpload.length) {
-      error.push('Incomplete Photo Uploads')
+      error.push('Incomplete Photo Uploads');
     }
-    if (error.length !== 1) {
-      alert(error.join('\n'))
+    if (error.length) {
+      setErrorMessage(error.join(', '));
+      setShowError(true);
     } else {
-      axios.post(`${Options.URL}/qa/questions/${props.question.question_id}/answers`, {
-        body: answer,
-        name: nickname,
-        email: email,
-        photos: photos
-      },
-      {
-        headers: {
-          Authorization: Options.TOKEN
-        }
-      })
-      .then(() => alert('Answer Submitted!'))
-      .then(() => props.setShowModal(false))
-      .then(() => props.refresh())
-      .catch(err => alert(err))
+      axios
+        .post(
+          `${Options.URL}/qa/questions/${props.question.question_id}/answers`,
+          {
+            body: answer,
+            name: nickname,
+            email: email,
+            photos: photos,
+          },
+          {
+            headers: {
+              Authorization: Options.TOKEN,
+            },
+          }
+        )
+        .then(() => {
+          alert('Answer Submitted!');
+          $('.modal-backdrop').remove();
+        })
+        .then(() => {
+          props.setShowModal(false);
+          props.refresh();
+        })
+        .catch((err) => alert(err));
     }
   }
 
   function handlePhotos(e) {
-    setPhotosToUpload(e.target.files)
-    handleUpload(e.target.files)
+    setPhotosToUpload(e.target.files);
+    handleUpload(e.target.files);
   }
 
   async function handleUpload(files) {
     if (files.length <= 5) {
-      let uploads = []
+      let uploads = [];
       for (let i = 0; i < files.length; i++) {
-        await axios.post('https://api.imgur.com/3/image', files[i],
-        {
-          headers: {
-            Authorization: "Client-ID 78dc8e1b5fb253b"
-          }
-        })
-        .then(res => {
-          uploads.push(res.data.data.link)
-        })
-        .catch(err => console.error(err))
+        await axios
+          .post('https://api.imgur.com/3/image', files[i], {
+            headers: {
+              Authorization: 'Client-ID 78dc8e1b5fb253b',
+            },
+          })
+          .then((res) => {
+            uploads.push(res.data.data.link);
+          })
+          .catch((err) => console.error(err));
       }
-      setPhotos(uploads)
+      setPhotos(uploads);
     } else {
-    alert('Too many files, limit is 5.')
+      setErrorMessage('Too many files, limit is 5');
+      setShowError(true);
     }
   }
 
   const modalStyle = {
-    backgroundColor: props.darkTheme ? "rgb(50, 50, 50)" : "white",
-    color: props.darkTheme ? "white" : "black"
-  }
+    backgroundColor: props.darkTheme ? 'rgb(50, 50, 50)' : 'white',
+    color: props.darkTheme ? 'white' : 'black',
+  };
 
   function handleClick(e) {
     if (e.target !== e.currentTarget) {
-      event.stopPropagation()
-      return
+      e.stopPropagation();
+      return;
     }
-    props.setShowModal(false)
+    props.setShowModal(false);
   }
 
-  function validateEmail(input) {
-    var validRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-    if (input.match(validRegex)) {
-      return true;
-    } else {
-      return false;
-    }
+  function handleClose() {
+    setShowError(false);
   }
 
   return (
-    <div className="modal fade" id="answerModal" tabIndex="-1" aria-hidden="true" onClick={(e) => handleClick(e)}>
-      <div className="modal-dialog">
-        <div className="modal-content" style={modalStyle}>
-          <div className="modal-header">
-            <h5 className="modal-title">Submit Your Answer: {question.question_body}</h5>
-            <button type="button" className="btn-close" onClick={() => props.setShowModal(false)} data-dismiss="modal" aria-label="Close"></button>
+    <div
+      className='modal fade'
+      id='answerModal'
+      tabIndex='-1'
+      aria-hidden='true'
+      onClick={(e) => handleClick(e)}
+    >
+      <div className='modal-dialog'>
+        <div className='modal-content' style={modalStyle}>
+          <Snackbar
+            open={showError}
+            autoHideDuration={5000}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            onClose={handleClose}
+          >
+            <Alert onClose={handleClose} severity='error'>
+              {errorMessage}
+            </Alert>
+          </Snackbar>
+          <div className='modal-header'>
+            <h5 className='modal-title'>
+              Submit Your Answer: {question.question_body}
+            </h5>
+            <button
+              type='button'
+              className='btn-close'
+              onClick={() => props.setShowModal(false)}
+              data-dismiss='modal'
+              aria-label='Close'
+            ></button>
           </div>
-          <div className="modal-body">
+          <div className='modal-body'>
             <form>
-              <div className="mb-3">
-                <label htmlFor="answer" className="col-form-label">Your Answer*:</label>
-                <textarea style={modalStyle} type="text" className="form-control" id="answer" maxLength={1000} onChange={e => setAnswer(e.target.value)}></textarea>
+              <div className='mb-3'>
+                <label htmlFor='answer' className='col-form-label'>
+                  Your Answer*:
+                </label>
+                <textarea
+                  style={modalStyle}
+                  type='text'
+                  className='form-control'
+                  id='answer'
+                  maxLength={1000}
+                  onChange={(e) => setAnswer(e.target.value)}
+                ></textarea>
               </div>
-              <div className="pt-3">
-                <label htmlFor="formFileMultiple" className="form-label">Upload Photos (Limit 5)</label>
-                <input style={modalStyle} className="form-control" type="file" id="formFileMultiple" multiple onChange={handlePhotos}/>
-                {photosToUpload.length > 0 && photosToUpload.length !== photos.length &&
-                <span>{photosToUpload.length <= 5 ? 'Uploading...' : 'Too Many Files'}</span>
-                }
-                {photos.length > 0 &&
-                <div>
-                  <span>Preview:</span><br />
-                  {photos.map(photo => <img key={photo} src={photo} height="50px" alt="Preview Photo" className="me-2"/>)}
-                </div>
-                }
+              <div className='pt-3'>
+                <label htmlFor='formFileMultiple' className='form-label'>
+                  Upload Photos (Limit 5)
+                </label>
+                <input
+                  style={modalStyle}
+                  className='form-control'
+                  type='file'
+                  id='formFileMultiple'
+                  multiple
+                  onChange={handlePhotos}
+                />
+                {photosToUpload.length > 0 &&
+                  photosToUpload.length !== photos.length && (
+                    <div className='flex'>
+                      {photosToUpload.length <= 5 ? (
+                        <Box sx={{ width: '100%' }}>
+                          <LinearProgress /> Uploading...
+                        </Box>
+                      ) : (
+                        'Too Many Files'
+                      )}
+                    </div>
+                  )}
+                {photos.length > 0 && (
+                  <div>
+                    <span>Preview:</span>
+                    <br />
+                    {photos.map((photo) => (
+                      <img
+                        key={photo}
+                        src={photo}
+                        height='50px'
+                        alt='Preview Photo'
+                        className='me-2'
+                      />
+                    ))}
+                  </div>
+                )}
               </div>
-              <div className="mb-3">
-                <label htmlFor="nickname" className="col-form-label">Nickname*:</label>
-                <input style={modalStyle} className="form-control" id="nickname" maxLength={60} placeholder="Example: jack543!" onChange={e => setNickname(e.target.value)}></input>
+              <div className='mb-3'>
+                <label htmlFor='nickname' className='col-form-label'>
+                  Nickname*:
+                </label>
+                <input
+                  style={modalStyle}
+                  className='form-control'
+                  id='nickname'
+                  maxLength={60}
+                  placeholder='Example: jack543!'
+                  onChange={(e) => setNickname(e.target.value)}
+                ></input>
                 For privacy reasons, do not use your full name or email address
               </div>
-              <div className="mb-3">
-                <label htmlFor="email" className="col-form-label">Email*:</label>
-                <input style={modalStyle} type="email" className="form-control" id="email" maxLength={60} placeholder="jack@email.com" onChange={e => setEmail(e.target.value)}></input>
+              <div className='mb-3'>
+                <label htmlFor='email' className='col-form-label'>
+                  Email*:
+                </label>
+                <input
+                  style={modalStyle}
+                  type='email'
+                  className='form-control'
+                  id='email'
+                  maxLength={60}
+                  placeholder='jack@email.com'
+                  onChange={(e) => setEmail(e.target.value)}
+                ></input>
                 For authentication reasons, you will not be emailed
               </div>
             </form>
           </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" onClick={() => props.setShowModal(false)} data-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary" onClick={handleSubmit} data-dismiss="modal">Submit Answer</button>
+          <div className='modal-footer'>
+            <button
+              type='button'
+              className='btn btn-secondary'
+              onClick={() => props.setShowModal(false)}
+              data-dismiss='modal'
+            >
+              Close
+            </button>
+            <button
+              type='button'
+              className='btn btn-primary'
+              onClick={handleSubmit}
+              // data-dismiss='modal'
+            >
+              Submit Answer
+            </button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/QuestionsAnswers/index.jsx
+++ src/components/QuestionsAnswers/index.jsx
@@ -1,68 +1,115 @@
-import React from "react"
-import Options from "../../config"
-import axios from "axios"
-import QAElement from "./QAElement"
-import AddQuestion from "./AddQuestion"
+import React from 'react';
+import Options from '../../config';
+import axios from 'axios';
+import QAElement from './QAElement';
+import AddQuestion from './AddQuestion';
+import { Alert, Snackbar } from '@mui/material';
 
 export default function QuestionsAnswers(props) {
+  const [productQuestions, setProductQuestions] = React.useState([]);
+  const [questionCount, setQuestionCount] = React.useState(2);
+  const [search, setSearch] = React.useState('');
+  const [showSnack, setShowSnack] = React.useState(false);
+  const [snackMessage, setSnackMessage] = React.useState('');
+  const [showModal, setShowModal] = React.useState(false);
 
-  const [productQuestions, setProductQuestions] = React.useState([])
-  const [questionCount, setQuestionCount] = React.useState(2)
-  const [search, setSearch] = React.useState('')
-
-  let questions = []
+  let questions = [];
 
   React.useEffect(() => {
-    fetchQuestions()
-  }, [props.selected])
+    fetchQuestions();
+  }, [props.selected]);
 
   function fetchQuestions() {
-    axios.get(`${Options.URL}/qa/questions/?product_id=${props.selected.id}&count=999`, {
-      headers: {
-        Authorization: Options.TOKEN
-      }
-    })
-      .then(res => setProductQuestions(res.data.results))
+    axios
+      .get(
+        `${Options.URL}/qa/questions/?product_id=${props.selected.id}&count=999`,
+        {
+          headers: {
+            Authorization: Options.TOKEN,
+          },
+        }
+      )
+      .then((res) => setProductQuestions(res.data.results));
+  }
+
+  function handleClose() {
+    setShowSnack(false);
   }
 
   if (search.length >= 3) {
-    questions = productQuestions.filter(question => question.question_body.toLowerCase().includes(search.toLowerCase()))
+    questions = productQuestions.filter((question) =>
+      question.question_body.toLowerCase().includes(search.toLowerCase())
+    );
   } else {
-    questions = productQuestions
+    questions = productQuestions;
   }
 
-  const QAElements = questions.slice(0, questionCount).map(element => (
-    <QAElement
-      element={element}
-      key={element.question_id}
-      refresh={fetchQuestions}
-      darkTheme={props.darkTheme}
-    />
-  ))
+  const QAElements = questions
+    .slice(0, questionCount)
+    .map((element) => (
+      <QAElement
+        element={element}
+        key={element.question_id}
+        refresh={fetchQuestions}
+        darkTheme={props.darkTheme}
+        setSnackMessage={setSnackMessage}
+        setShowSnack={setShowSnack}
+      />
+    ));
 
-  const buttonStyle = props.darkTheme ? "btn btn-outline-light" : "btn btn-outline-dark"
+  const buttonStyle = props.darkTheme
+    ? 'btn btn-outline-light'
+    : 'btn btn-outline-dark';
 
   const searchStyle = {
-    borderColor: props.darkTheme ? "white" : "black",
-    color: props.darkTheme ? "white" : "black",
-  }
+    borderColor: props.darkTheme ? 'white' : 'black',
+    color: props.darkTheme ? 'white' : 'black',
+  };
 
   return (
     <>
-      <div className='sectionTitle pt-5'>
-        {`QUESTIONS & ANSWERS`}
+      <div className='sectionTitle pt-5'>{`QUESTIONS & ANSWERS`}</div>
+      <Snackbar
+        open={showSnack}
+        autoHideDuration={5000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity='info'>
+          {snackMessage}
+        </Alert>
+      </Snackbar>
+      <div className='pt-3'>
+        <input
+          className='w-100 searchBar'
+          style={searchStyle}
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          placeholder='Have a question? Search for answers...'
+        ></input>
       </div>
-      <div className="pt-3">
-        <input className="w-100 searchBar" style={searchStyle} onChange={e => setSearch(e.target.value)} value={search} placeholder="Have a question? Search for answers..."></input>
-      </div>
-      <div className="pt-2 mb-2 questionsColumn">
-        {QAElements}
-      </div>
-      {questionCount === 2 &&
-      <button onClick={() => setQuestionCount(999)} className={buttonStyle}>More Questions</button>
-      }
-      <button className={`ms-3 ${buttonStyle}`} data-bs-toggle="modal" data-bs-target="#questionModal">+ Add a Question</button>
-      <AddQuestion selected={props.selected} refresh={fetchQuestions} darkTheme={props.darkTheme}/>
+      <div className='pt-2 mb-2 questionsColumn'>{QAElements}</div>
+      {questionCount === 2 && (
+        <button onClick={() => setQuestionCount(999)} className={buttonStyle}>
+          More Questions
+        </button>
+      )}
+      <button
+        className={`ms-3 ${buttonStyle}`}
+        data-toggle='modal'
+        data-target='#questionModal'
+        onClick={() => setShowModal(true)}
+      >
+        + Add a Question
+      </button>
+      {showModal && (
+        <AddQuestion
+          selected={props.selected}
+          refresh={fetchQuestions}
+          darkTheme={props.darkTheme}
+          setShowModal={setShowModal}
+        />
+      )}
     </>
-  )
+  );
 }
