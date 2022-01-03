688e496cabf19eb64b6c09b4464238022f305f54
emilyhu08 <emilyhu08@gmail.com>
2022-01-03 10:51:18 -0800

    fixed dropdown console warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Checkout from "./Checkout";
 import Catalogue from "./Catalogue";
 import Options from "../config";
