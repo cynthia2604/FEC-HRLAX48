aea5a05dcea595d8f0e0eef1e136bc9144949fce
milesmaroney <miles@milesmaroney.com>
2022-01-05 10:12:45 -0800

    Updated style for customButton class, stars able to be vertically centered, if statement on get request for related

--- src/components/RelatedItems/OutfitList/OutfitListEntry.jsx
+++ src/components/RelatedItems/OutfitList/OutfitListEntry.jsx
@@ -1,57 +1,86 @@
 import React from 'react';
-import OutfitListImage from './OutfitListImage'
-import RelatedItemRating from '../RelatedItemRating'
-import axios from 'axios'
+import OutfitListImage from './OutfitListImage';
+import RelatedItemRating from '../RelatedItemRating';
+import axios from 'axios';
 import Options from '../../../config.js';
-import utils from '../../utils.js'
+import utils from '../../utils.js';
 
-export default function OutListEntry (props) {
-  const [currentRating, setCurrentRating] = React.useState()
+export default function OutListEntry(props) {
+  const [currentRating, setCurrentRating] = React.useState();
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     const params = {
-      product_id: props.currentStyle.productId
+      product_id: props.currentStyle.productId,
+    };
+    if (params.product_id) {
+      axios
+        .get(`${Options.URL}/products/${params.product_id}/styles/?count=20`, {
+          headers: {
+            Authorization: Options.TOKEN,
+          },
+        })
+        .then((res) => setCurrentRating(res.data));
     }
-    axios.get(`${Options.URL}/products/${params.product_id}/styles/?count=20`, {
-      headers: {
-        Authorization: Options.TOKEN
-      }
-    }).then(res => setCurrentRating(res.data))
-  }, [props.currentStyle])
+  }, [props.currentStyle]);
 
   return (
     <>
-    {(props.currentStyle && currentRating) &&
-      <div className= "outfit-card-entry" style={{width: `${(props.outfits.length/4)*30}%` }}>
-        <OutfitListImage
-          setSaved={props.setSaved}
-          outfits={props.outfits}
-          setSelectRelated={props.setSelectRelated}
-          selectedStyle={props.selectedStyle}
-          currentStyle={props.currentStyle}
-        />
-        <div className="user-product-category" style={{fontSize: '0.8em', marginLeft: '10px'}}>{props.currentStyle.color}</div>
-        <div className="user-product-name" style={{fontWeight:'bold', marginLeft: '10px', overflow:'hidden'}}>{props.currentStyle.productName}</div>
-        {props.currentStyle.salePrice ? (
-             <div className="user-product-price" style={{fontSize: '0.8em', marginLeft: '10px'}}>
-                <span>
-                  <s>{`$${props.currentStyle.originalPrice}`}</s>
-                </span>
-                <span style={{ color: "red" }} className="ms-2">
-                  {`$${props.currentStyle.salePrice}`}
-                </span>
-              </div>
-        ) : (
-              <div className="user-product-price" style={{fontSize: '0.8em', marginLeft: '10px'}}>
-               {`$${props.currentStyle.originalPrice}`}
-              </div>
-            )
-        }
-        <RelatedItemRating currentItem={currentRating} darkTheme={props.darkTheme}/>
-      </div>
-    }
+      {props.currentStyle && currentRating && (
+        <div
+          className='outfit-card-entry'
+          style={{ width: `${(props.outfits.length / 4) * 30}%` }}
+        >
+          <OutfitListImage
+            setSaved={props.setSaved}
+            outfits={props.outfits}
+            setSelectRelated={props.setSelectRelated}
+            selectedStyle={props.selectedStyle}
+            currentStyle={props.currentStyle}
+          />
+          <div
+            className='user-product-category'
+            style={{ fontSize: '0.8em', marginLeft: '10px' }}
+          >
+            {props.currentStyle.color}
+          </div>
+          <div
+            className='user-product-name'
+            style={{
+              fontWeight: 'bold',
+              marginLeft: '10px',
+              overflow: 'hidden',
+            }}
+          >
+            {props.currentStyle.productName}
+          </div>
+          {props.currentStyle.salePrice ? (
+            <div
+              className='user-product-price'
+              style={{ fontSize: '0.8em', marginLeft: '10px' }}
+            >
+              <span>
+                <s>{`$${props.currentStyle.originalPrice}`}</s>
+              </span>
+              <span style={{ color: 'red' }} className='ms-2'>
+                {`$${props.currentStyle.salePrice}`}
+              </span>
+            </div>
+          ) : (
+            <div
+              className='user-product-price'
+              style={{ fontSize: '0.8em', marginLeft: '10px' }}
+            >
+              {`$${props.currentStyle.originalPrice}`}
+            </div>
+          )}
+          <RelatedItemRating
+            currentItem={currentRating}
+            darkTheme={props.darkTheme}
+          />
+        </div>
+      )}
     </>
-  )
+  );
 }
 //   {selected.salePrice ? (
 //     <div>
