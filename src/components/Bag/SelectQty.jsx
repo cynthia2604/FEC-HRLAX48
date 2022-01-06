7af3ffd665bcaa8d5103b142da81ae0142229efe
milesmaroney <miles@milesmaroney.com>
2022-01-05 18:20:29 -0800

    Bag quantity progress

--- src/components/Bag/SelectQty.jsx
+++ src/components/Bag/SelectQty.jsx
@@ -1,34 +1,36 @@
-import React from "react";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import { v4 as uuidv4 } from "uuid";
-import { useStateValue } from "../Overview/store/StateProvider";
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { v4 as uuidv4 } from 'uuid';
+import { useStateValue } from '../Overview/store/StateProvider';
 
 export default function SelectQty({ qtyArr, item, darkTheme }) {
   //const [selectedQty, setSelectedQty] = React.useState(item.quantity);
   const [{ basket, selected }, dispatch] = useStateValue();
   const handleSelectQty = (qty) => {
     //qty===4
-    let newBasket = basket;
-    for (var bagItem of basket) {
-      if (item.id === bagItem.id && item.size === bagItem.size) {
-        newBasket.quantity = qty;
-        newBasket.originalPrice = qty * bagItem.originalPrice;
-        newBasket.salePrice = qty * bagItem.salePrice;
+    let newBasket = [...basket];
+    for (let i = 0; i < newBasket.length; i++) {
+      if (newBasket[i].id === item.id && newBasket[i].size === item.size) {
+        newBasket[i].quantity = qty;
+        // newBasket[i].originalPrice = qty * item.originalPrice;
+        // newBasket[i].salePrice = qty * item.salePrice;
+        newBasket[i].totalPrice = qty * (item.salePrice || item.originalPrice);
       }
     }
+
     dispatch({
-      type: "REPLACE_BASKET",
+      type: 'REPLACE_BASKET',
       item: newBasket,
     });
   };
 
   return (
     <DropdownButton
-      id="dropdown-basic-button qty"
-      variant={darkTheme ? "outline-light" : "outline-secondary"}
+      id='dropdown-basic-button qty'
+      variant={darkTheme ? 'outline-light' : 'outline-secondary'}
       title={item.quantity}
-      size="sm"
+      size='sm'
     >
       {qtyArr &&
         qtyArr.map((qty) => {
@@ -39,7 +41,7 @@ export default function SelectQty({ qtyArr, item, darkTheme }) {
                   handleSelectQty(qty);
                 }}
                 key={uuidv4()}
-                className={darkTheme && "dropdown-item-dark"}
+                className={darkTheme && 'dropdown-item-dark'}
               >
                 {qty}
               </Dropdown.Item>
