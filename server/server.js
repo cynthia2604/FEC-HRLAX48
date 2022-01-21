e342ec0b70e0826b37ac4c4cb1ebe5bec20a6b98
“Cynthia <xin2604@gmail.com>
2022-01-21 12:09:33 -0800

    add loader.io get request

--- server/server.js
+++ server/server.js
@@ -9,4 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
+// verify for loader.io test
+// app.get('loader.io txt', (req, res) => {
+//   res.send('content');
+// })
 app.listen(port, () => console.log('Listening on 3001'));
